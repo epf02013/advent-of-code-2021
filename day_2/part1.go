042a779bbcefdbfdf59425a0ca551cad00f11fc0
Ethan Prater Fahey <ecpraterfahey@gmail.com>
2021-12-01 22:08:13 -0800

    feat(2.1): golang implementation

---
+++ day_2/part1.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Vector struct {
+	depth int
+	horizontal int
+}
+
+func (v1 Vector) add(v2 Vector) Vector {
+	return Vector{
+		depth: v1.depth+v2.depth,
+		horizontal: v1.horizontal+v2.horizontal,
+	}
+}
+
+func parseVector(command string) Vector {
+	parts := strings.Split(command, " ")
+	commandType := parts[0]
+	distance, _ := strconv.Atoi(parts[1])
+	if commandType == "forward"{
+		return Vector{depth: 0, horizontal: distance}
+	}
+	if commandType == "down"{
+		return Vector{depth: distance, horizontal: 0}
+	}
+	return Vector{depth: -distance, horizontal: 0}
+}
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Printf("error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	scanner := bufio.NewScanner(f)
+	currentPosition := Vector{depth: 0, horizontal: 0}
+	for scanner.Scan() {
+		vector := parseVector(scanner.Text())
+		currentPosition = currentPosition.add(vector)
+	}
+	fmt.Println(currentPosition.horizontal * currentPosition.depth)
+}
