539474e1b5b6180617a2912885916a7860ff97f0
Ethan Prater Fahey <ecpraterfahey@gmail.com>
2021-12-09 23:18:01 -0800

    feat: add stack in go

---
+++ day_10/stack.go
@@ -0,0 +1,25 @@
+package main
+
+type Stack struct {
+	items []string
+}
+
+func (stack Stack) pop() (Stack, string, error) {
+	return Stack{
+		items: stack.items[0:len(stack.items)-1],
+	}, stack.items[len(stack.items)-1], nil
+}
+
+func (stack Stack) peek() (string) {
+	return stack.items[len(stack.items)-1]
+}
+
+func (stack Stack) push(s string) Stack {
+	return Stack{
+		items: append(stack.items, s),
+	}
+}
+
+func (stack Stack) size() int {
+	return len(stack.items)
+}
--- day_10/stack_test.go
+++ day_10/stack_test.go
@@ -1,1 +1,33 @@
-package day_10
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPop(t *testing.T) {
+	stack := Stack{items: []string{"<","{"}}
+	res,item,_ := stack.pop()
+	assert.Equal(t, []string{"<"},res.items, "It should slice the items")
+	assert.Equal(t,  "{",item, "It should return the final element")
+}
+
+func TestPeek(t *testing.T) {
+	stack := Stack{items: []string{"<","{"}}
+	item := stack.peek()
+	assert.Equal(t,  "{",item, "It should return the final element")
+}
+
+func TestPush(t *testing.T) {
+	stack := Stack{items: []string{"<"}}
+	res:= stack.push("{")
+	assert.Equal(t, []string{"<", "{"},res.items, "It should append the string to items")
+}
+
+func TestSize(t *testing.T) {
+	stack := Stack{items: []string{"<", "{"}}
+	size:= stack.size()
+	assert.Equal(t, 2,size, "It should return the number of items")
+}
+
+
